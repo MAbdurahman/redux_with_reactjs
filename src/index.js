02c25f7c329a7db15e0b62eefb19bc797886c5d8
MAbdurahman <mdbdrrhm@gmail.com>
2022-05-06 16:38:59 -0500

    Complete react-router-dom, Links, NavLink, BrowswerRouter, Switch, and Route

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
-import App from './app/App'
+import App from './app/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
+root.render(<App />);
